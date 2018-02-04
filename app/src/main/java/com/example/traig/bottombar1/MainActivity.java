ce79d64b89b57416bf3cebde3978ba1dfeaf00d8
TaiDev198 <traigiuxom9x@gmail.com>
2018-02-05 06:49:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/traig/bottombar1/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.traig.bottombar1;
+
+import android.content.ClipData;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private MenuView.ItemView itemView;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //mTextMessage.setText(R.string.title_home);
+                  //  View v = View.inflate()
+                    Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_notifications1:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_notifications2:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        itemView = findViewById(R.id.navigation_home);
+        mTextMessage =  findViewById(R.id.message);
+        BottomNavigationView navigation =  findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
